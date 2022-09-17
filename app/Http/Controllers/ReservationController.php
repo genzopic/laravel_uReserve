bc760bb6568d1f8657bc1423fe8744acd437946a
genzo <genzo.pic@gmail.com>
2022-09-17 10:41:25 +0900

    reservation detail1

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class ReservationController extends Controller
+{
+    //
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function detail($id)
+    {
+        $event = Event::findOrFail($id);
+
+        return view('event-detail',compact('event'));
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
 use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,10 +33,8 @@ Route::prefix('manager')
 
 Route::middleware('can:user-higher')
         ->group(function(){
-            // /index
-            Route::get('index', function () {
-                dd('user');
-            });
+            Route::get('/dashboard', [ReservationController::class,'dashboard'])->name('dashboard');
+            Route::get('/{id}', [ReservationController::class,'detail'])->name('events.detail');
     });
 
 Route::controller(LivewireTestController::class)
@@ -53,12 +52,12 @@ Route::get('/', function () {
     return view('calendar');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//         'auth:sanctum',
+//         config('jetstream.auth_session'),
+//         'verified'
+//         ])->group(function () {
+//             Route::get('/dashboard', function () {
+//                 return view('dashboard');
+//             })->name('dashboard');
+//     });
